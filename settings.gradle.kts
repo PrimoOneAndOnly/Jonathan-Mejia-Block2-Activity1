e6866daf9ddb62da87ef998d0d44d808c405c704
PrimoOneAndOnly <onca.mejia.up@phinmaed.com>
2023-12-09 16:28:44 +0800

    14

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JonathanLorenzC.Mejia-Block2-Quiz"
+include(":app")
